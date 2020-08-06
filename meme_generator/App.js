e43cd7aaa8620026aa742f7315a237510aac3c67
Arsal github <arsahlahn3@gmail.com>
2020-08-06 17:29:46 +0500

    Add files via upload

---
+++ meme_generator/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+import Header from "./components/Header"
+import MemeGenerator from "./components/MemeGenerator"
+
+function App(){
+    return(
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
+    )
+}
+
+export default App
---
+++ meme_generator/components/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+
+function Header(){
+    return(
+        <header>
+            <img src = "../images/trollface.png" alt = "Problem!" />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
